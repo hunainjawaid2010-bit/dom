5790eccea419face645f61c424b23e3ba8e011d2
hunainjawaid2010-bit <hunain.jawaid2010@gmail.com>
2026-06-01 16:24:31 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,71 +1,24 @@
-let totalStudents = 0;
+function domath(operation) {
+    let num1 = number(document.getElementById("n1").value);
+    let num2 = number(document.getElementById("n2").value);
+    let result;
 
-function calculateGrade(percentage){
-
-    if(percentage >= 80){
-        return "A+";
+    if(operator === '+'){
+        result = num1 + num2;
+        console.log("result:" + num1 + "+" + num2 + "=" + result);
     }
-    else if(percentage >= 70){
-        return "A";
+    else if(operation ==='-'){
+        result = num1 - num2;
+        console.log("result:" + num1 + "-" + num2 + "=" + result);
     }
-    else if(percentage >= 60){
-        return "B";
+     else if(operation ==='-'){
+        result = num1 - num2;
+        console.log("result:" + num1 + "*" + num2 + "=" + result);
     }
-    else if(percentage >= 50){
-        return "C";
+         else if(operation ==='-'){
+        result = num1 - num2;
+        console.log("result:" + num1 + "/" + num2 + "=" + result);
     }
-    else{
-        return "Fail";
-    }
+    let x=5;
+    console.log( x +=5)
 }
-
-function addStudent(){
-
-    let name = document.getElementById("name").value;
-    let marks = document.getElementById("marks").value;
-
-    if(name === "" || marks === ""){
-        alert("Please fill all fields");
-        return;
-    }
-
-    let percentage = marks + "%";
-    let grade = calculateGrade(marks);
-
-    let table = document.getElementById("resultTable");
-
-    let row = table.insertRow();
-
-    row.innerHTML = `
-        <td>${name}</td>
-        <td>${marks}</td>
-        <td>${percentage}</td>
-        <td>${grade}</td>
-        <td>
-            <button class="delete-btn"
-            onclick="deleteStudent(this)">
-            Delete
-            </button>
-        </td>
-    `;
-
-    totalStudents++;
-    document.getElementById("count").innerText = totalStudents;
-
-    document.getElementById("name").value = "";
-    document.getElementById("marks").value = "";
-}
-
-function deleteStudent(btn){
-
-    btn.parentElement.parentElement.remove();
-
-    totalStudents--;
-
-    document.getElementById("count").innerText = totalStudents;
-}
-    
- 
-    
-
-
